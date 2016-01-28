7e7718fe48fd0d8b8b2a486fb7bc71ed7e035fa4
Jacob Rothstein <hi@jbr.me>
2016-01-27 18:02:12 -0800

    update with latest sibilant

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 var webpack = require("webpack");
-module.exports = {
-  entry: {
-    index: "./index.sibilant",
-    worker: "./worker.sibilant"
-  },
-  output: {
-    path: "build",
-    filename: "[name].bundle.js",
-    chunkFilename: "[id].[hash].bundle.js",
-    sourceMapFilename: "[file].map"
-  },
-  plugins: [ (new webpack.optimize.UglifyJsPlugin()) ],
-  devtool: "source-map",
-  module: { loaders: [ {
-    test: (new RegExp("\\.json$", undefined)),
-    loader: "json"
-  }, {
-    test: (new RegExp("\\.sibilant$", undefined)),
-    loader: "sibilant"
-  } ] }
+module.exports.entry = {
+  index: "./index.sibilant",
+  worker: "./worker.sibilant"
 };
+module.exports.output = {
+  path: "build",
+  filename: "[name].bundle.js",
+  chunkFilename: "[id].[hash].bundle.js",
+  sourceMapFilename: "[file].map"
+};
+module.exports.devtool = "eval";
+module.exports.module = { loaders: [ {
+  test: (new RegExp("\\.json$", undefined)),
+  loader: "json"
+}, {
+  test: (new RegExp("\\.sibilant$", undefined)),
+  loader: "sibilant"
+} ] };
+(function() {
+  if ("production" === process.env.NODE_ENV) {
+    module.exports.devtool = "source-map";
+    return module.exports.plugins = [ (new webpack.optimize.UglifyJsPlugin()), (new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") })) ];
+  }
+}).call(this);
